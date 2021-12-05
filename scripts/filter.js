ffcd1a95bcb04d38741b6d4f9278cc141ba69c7d
Mandy H <flyingpanduh@gmail.com>
2021-12-05 15:16:47 -0500

    Added logic to refresh the tag data after 14 days

--- scripts/filter.js
+++ scripts/filter.js
@@ -43,14 +43,10 @@ window.addEventListener('DOMContentLoaded', function () {
      * @returns {Promise}
      */
     processLargeArray(array, cb, maxTimePerChunk = 100, index = 0) {
-      function now() {
-        return new Date().getTime();
-      }
-
       function processChunk() {
-        const startTime = now();
+        const startTime = Date.now();
         const fragment = new DocumentFragment();
-        while (index < array.length && (now() - startTime) <= maxTimePerChunk) {
+        while (index < array.length && (Date.now() - startTime) <= maxTimePerChunk) {
           const row = cb(array[index], index, array);
           row && fragment.appendChild(row);
           index++;
@@ -201,15 +197,11 @@ window.addEventListener('DOMContentLoaded', function () {
     },
 
     clearTable() {
-      function now() {
-        return new Date().getTime();
-      }
-
       function removeRows() {
-        const startTime = now();
+        const startTime = Date.now();
         const maxTimePerChunk = 100;
         let rowCount = CONSTANTS.elements.tableBody.children.length;
-        while (rowCount > 1 && (now() - startTime) <= maxTimePerChunk) {
+        while (rowCount > 1 && (Date.now() - startTime) <= maxTimePerChunk) {
           CONSTANTS.elements.tableBody.removeChild(CONSTANTS.elements.tableBody.lastChild);
           rowCount--;
         }
@@ -241,6 +233,26 @@ window.addEventListener('DOMContentLoaded', function () {
   /* ========== Main code ========== */
 
   (function init() {
+    // Get adoptable guide tags
+    const p = filterUtils.getStorageData(['adoptableGuideTags', 'tagDataLastRefreshed'])
+      .then((data) => {
+        // Return another promise to return the resolved value to the .then() in the chain
+        return new Promise((resolve, reject) => {
+          const expirationDuration = 1000 * 60 * 60 * 24 * 14; // 14 days
+          const currentTime = Date.now();
+          const refreshData = currentTime - data.tagDataLastRefreshed > expirationDuration;
+          if (!data.adoptableGuideTags || refreshData) {
+            filterUtils.fetchTags().then((tags) => {
+              chrome.storage.local.set({ 'adoptableGuideTags': tags });
+              chrome.storage.local.set({ 'tagDataLastRefreshed': currentTime });
+              resolve(tags);
+            });
+          } else {
+            resolve(data.adoptableGuideTags);
+          }
+        });
+      });
+
     // Append form elements to page
     const filterWrapper = document.createElement('div');
     filterWrapper.id = 'collection-filter';
@@ -300,19 +312,7 @@ window.addEventListener('DOMContentLoaded', function () {
     `;
     document.querySelector('#megaContent center').insertBefore(filterWrapper, document.querySelector('.niceTable'));
 
-    // Get adoptable guide tags
-    filterUtils.getStorageData('adoptableGuideTags').then((data) => {
-      return new Promise((resolve, reject) => {
-        if (Object.keys(data).length === 0) {
-          filterUtils.fetchTags().then((tags) => {
-            chrome.storage.local.set({ 'adoptableGuideTags': tags });
-            resolve(tags);
-          });
-        } else {
-          resolve(data.adoptableGuideTags);
-        }
-      });
-    }).then((tags) => {
+    p.then((tags) => {
       // Append tags to dropdown
       const fragment = new DocumentFragment();
       tags.forEach((tag) => {
